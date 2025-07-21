9b2ef58cb76a65f937f95b25704866d8b0a58c06
Iarley Souza <iarleysouza615@gmail.com>
2025-07-21 20:32:21 -0300

    add enum

---
+++ src/main/java/br/com/exemplo/ScrenMatch/model/Serie.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.ScrenMatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public class Serie {
+    private String Titulo;
+    private Integer totalTemporada;
+    private Double avaliacao;
+    private Categoria genero;
+    private String stores;
+    private String poster;
+    private String sinopse;
+}
