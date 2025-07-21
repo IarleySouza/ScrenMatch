8669dbb91f64ee793ea022e973ccefee2c9e071d
Iarley Souza <iarleysouza615@gmail.com>
2025-07-21 20:59:29 -0300

    add enum

--- src/main/java/br/com/exemplo/ScrenMatch/model/Categoria.java
+++ src/main/java/br/com/exemplo/ScrenMatch/model/Categoria.java
@@ -1,4 +1,26 @@
 package br.com.exemplo.ScrenMatch.model;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public enum Categoria {
+    ACAO("Action"),
+    ROMANCE("Romance"),
+    COMEDIA("Comedy"),
+    DRAMA("Drama"),
+    CRIME("Crime");
+
+    private String categoriaOmdb;
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
+
 }
--- src/main/java/br/com/exemplo/ScrenMatch/model/Serie.java
+++ src/main/java/br/com/exemplo/ScrenMatch/model/Serie.java
@@ -1,13 +1,29 @@
 package br.com.exemplo.ScrenMatch.model;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.OptionalDouble;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Serie {
-    private String Titulo;
+    private String titulo;
     private Integer totalTemporada;
     private Double avaliacao;
     private Categoria genero;
     private String stores;
     private String poster;
     private String sinopse;
+
+    public Serie(DadosSeries dadosSeries) {
+        this.titulo = dadosSeries.Titulo();
+        this.totalTemporada = dadosSeries.totalTemporada();
+        this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSeries.avaliacao())).orElse(0);
+        this.genero = Categoria.fromString(dadosSeries.genero().split(",")[0].trim());
+    }
 }
